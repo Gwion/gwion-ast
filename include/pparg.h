99c8a6ee64cb973c516a89c329bb953e3f13599a
fennecdjay <fennecdjay@gwion.tk>
2020-11-07 12:28:38 +0100

    :art: Update macro arguments system

--- include/pparg.h
+++ include/pparg.h
@@ -26,4 +26,11 @@ ANN static inline int ppa_rem_macro(struct PPArg_* ppa, const Symbol sym) {
   return macro_has(&ppa->hash, s_name(sym)) ? 0 : GW_ERROR;
 }
 
+ANN2(1) m_bool pparg_add(struct PPArg_ *ppa, const m_str str);
+ANN2(1) static inline m_bool pparg_rem(struct PPArg_ *ppa, const m_str str) {
+  return macro_rem(&ppa->hash, str);
+}
+ANN static inline void pparg_inc(struct PPArg_ *ppa, const m_str str) {
+  return vector_add(&ppa->path, (vtype)str);
+}
 #endif
--- src/pparg.c
+++ src/pparg.c
@@ -1,22 +1,6 @@
 #include "gwion_util.h"
 #include "gwion_ast.h"
 
-static const char usage[] =
-"usage: gwion-lint <options>\n"
-"  -D    <macro>   : add a macro\n"
-"  -U     <xid>    : remove a macro\n"
-"  -I     <path>   : add to include path\n"
-"  -h     <path>   : this help\n";
-
-ANN static inline m_str get_arg(Vector v, m_uint *i) {
-  const m_str arg = (m_str)vector_at(v, *i);
-  const char key = arg[1];
-  const m_str str = arg[2] == '\0' ? (m_str)vector_at(v, ++(*i)) : arg + 2;
-  if(!str)
-    gw_err(_("option '-%c' needs arguments\n"), key);
-  return str;
-}
-
 ANN static Macro pparg_def(struct PPArg_ *ppa, const m_str str) {
   const ssize_t sz = strlen(str);
   char base[sz + 1];
@@ -58,7 +42,9 @@ ANN static GwText* pparg_body(struct PPArg_ *ppa, const m_str str) {
   return text;
 }
 
-ANN2(1) static m_bool pparg_add(struct PPArg_ *ppa, const m_str str) {
+ANN2(1) m_bool pparg_add(struct PPArg_ *ppa, const m_str str) {
+  if(!ppa->hash.table)
+    hini(&ppa->hash, 127);
   DECL_OB(const Macro, m, = pparg_def(ppa, str))
   const m_str arg = strchr(str, '(');
   if(arg)
@@ -68,47 +54,6 @@ ANN2(1) static m_bool pparg_add(struct PPArg_ *ppa, const m_str str) {
     m->text = pparg_body(ppa, body + 1);
   m->file = "command line";
   return GW_OK;
-}
-
-ANN static inline m_bool pparg_err(void) {
-  gw_err(_("invalid arguments"));
-  return GW_ERROR;
-}
-
-ANN static m_bool _pparg_run(struct PPArg_ *ppa, const Vector v) {
-  for(m_uint i = 0; i < vector_size(v); ++i) {
-    const m_str base = (m_str)vector_at(v, i);
-    if(base[0] == '-') {
-      m_str arg;
-      CHECK_OB((arg = get_arg(v, &i)))
-      switch(base[1]) {
-        case 'D': // define
-          if(!ppa->hash.table)
-            hini(&ppa->hash, 127);
-          pparg_add(ppa, arg);
-          break;
-        case 'U': // undef
-          CHECK_BB(macro_rem(&ppa->hash, arg))
-          break;
-        case 'I': // include
-          vector_add(&ppa->path, (vtype)arg);
-          break;
-        case 'h': // help
-          puts(usage);
-          break;
-        default:
-          pparg_err();
-      }
-    } else
-      pparg_err();
-  }
-  return GW_OK;
-}
-
-ANN2(1) m_bool pparg_run(struct PPArg_ *ppa, const Vector v) {
-  if(!v)
-    return GW_OK;
-  return _pparg_run(ppa, v);
 }
 
 ANN void pparg_ini(MemPool mp, struct PPArg_* a) {
