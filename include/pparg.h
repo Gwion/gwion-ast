bea5d76b2bdb10a0cbc68b0549d1202db7e5f9e6
fennecdjay <fennecdjay@gwion.tk>
2020-11-13 14:05:42 +0100

    :wrench: Test preprocessor options

--- include/pparg.h
+++ include/pparg.h
@@ -28,6 +28,8 @@ ANN static inline int ppa_rem_macro(struct PPArg_* ppa, const Symbol sym) {
 
 ANN2(1) m_bool pparg_add(struct PPArg_ *ppa, const m_str str);
 ANN2(1) static inline m_bool pparg_rem(struct PPArg_ *ppa, const m_str str) {
+  if(!ppa->hash.table)
+    return GW_ERROR;
   return macro_rem(&ppa->hash, str);
 }
 ANN static inline void pparg_inc(struct PPArg_ *ppa, const m_str str) {
